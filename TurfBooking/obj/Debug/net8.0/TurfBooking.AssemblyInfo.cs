c080b5af5a18919601be4fd5775d85db7f121da2
Rhythem Malviya <rhythemmalviya789@gmail.com>
2024-05-30 12:53:25 +0530

    changes by the commit

--- TurfBooking/obj/Debug/net8.0/TurfBooking.AssemblyInfo.cs
+++ TurfBooking/obj/Debug/net8.0/TurfBooking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurfBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c732146f5e7c7f619646db2a970ff524aeafcd9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0826526b8f473fcc644665b89a7dfbe399437003")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurfBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurfBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
